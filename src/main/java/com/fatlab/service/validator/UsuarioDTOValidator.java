e5394870d1f036d846cb8a78c41070d166b7704b
BrendonPrado <brendonmatheus123@gmail.com>
2019-04-17 18:57:42 -0300

    feito usuarioDTOvalidator

---
+++ src/main/java/com/fatlab/service/validator/UsuarioDTOValidator.java
@@ -0,0 +1,50 @@
+package com.fatlab.service.validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fatlab.domain.Aluno;
+import com.fatlab.domain.Professor;
+import com.fatlab.dto.UsuarioDTO;
+import com.fatlab.repositories.AlunoRepository;
+import com.fatlab.repositories.ProfessorRepository;
+import com.fatlab.resource.exception.FieldMessage;
+
+public class UsuarioDTOValidator implements ConstraintValidator<UsuarioDTOValido,UsuarioDTO > {
+
+	@Autowired
+	private AlunoRepository alunoRep;
+
+	@Autowired
+	private ProfessorRepository profRep;
+	
+	@Override
+	public boolean isValid(UsuarioDTO value, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		Aluno aluno = alunoRep.findAlunoByRa(value.getMatricula());
+		Professor prof = profRep.findProfessorByMatricula(value.getMatricula());
+		
+		boolean usuarioPreenchido = (aluno != null ) || (prof != null);
+		if(usuarioPreenchido ) {
+			list.add(new FieldMessage("matricula","este usuário já possui cadastro, insira um ra válido!"));
+		}
+		
+		if(!value.getTipo().equals("Aluno") && !value.getTipo().equals("Professor")) {
+			list.add(new FieldMessage("tipo","Por favor cadastre um tipo válido de usuario"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+
+}
---
+++ src/main/java/com/fatlab/service/validator/UsuarioDTOValido.java
@@ -0,0 +1,22 @@
+package com.fatlab.service.validator;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Constraint(validatedBy = UsuarioDTOValidator.class)
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface UsuarioDTOValido {
+	String message() default "Erro de validação";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+    
+}
